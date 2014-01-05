f9eeb761dc5b2e5f138b96e685778dd55517c87e
paulmakl <paul@paulnmakl.com>
2014-01-05 14:33:05 -0500

    showin Etan Stuff

---
+++ test.cpp
@@ -0,0 +1,9 @@
+//
+//  test.cpp
+//  Capstone
+//
+//  Created by Paul Makl on 1/5/14.
+//  Copyright (c) 2014 Paul Makl. All rights reserved.
+//
+
+#include "test.h"
---
+++ test.h
@@ -0,0 +1,14 @@
+//
+//  test.h
+//  Capstone
+//
+//  Created by Paul Makl on 1/5/14.
+//  Copyright (c) 2014 Paul Makl. All rights reserved.
+//
+
+#ifndef __Capstone__test__
+#define __Capstone__test__
+
+#include <iostream>
+
+#endif /* defined(__Capstone__test__) */
