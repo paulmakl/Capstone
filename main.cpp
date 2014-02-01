beac52c243d0c18cf43700506f4f2a196a86ce15
Etan Bard <ebard@pugetsound.edu>
2014-01-31 16:31:11 -0800

    Force Interpolation Framework
    Particles will not inherit their velocities based on the nearby nodes,
    though they are not interpolating the forces. Code was also cleaned up.

--- main.cpp
+++ main.cpp
@@ -5,17 +5,19 @@
  */
 
 #ifdef __APPLE__
-#include <GLUT/glut.h>
+//#include <GLUT/glut.h>
 #else
 #include <GL/freeglut.h>  // GLUT, includes glu.h and gl.h
 #include <windows.h>
 #endif
 
 #include <time.h>
+#include <string>
+#include <cmath>
 
 #define xSize 64
 #define ySize 64
-#define numParticles 2000
+#define numParticles 10000
 
 struct Vec2
 {
@@ -92,26 +94,14 @@ public:
 Node grid[xSize][ySize];
 Particle particles[numParticles];
 
-void setGrid(Node* newGrid[xSize][ySize])
+void timer(int id)
 {
-	int x, y;
-	for(x = 0; x < xSize; x++)
-	{
-		for(y = 0; y < ySize; y++)
-		{
-			grid[x][y] = *newGrid[x][y];
-		}
-	}
+	glutPostRedisplay();
 }
 
-/* Handler for window-repaint event. Call back when the window first appears and
-    whenever the window needs to be re-painted. */
-static void display()
+void displayGrid()
 {
-	//glClearDepth(GL_DEPTH_TEST);
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color to black and opaque
-	glClear(GL_COLOR_BUFFER_BIT);         // Clear the color buffer
-
+	glPointSize(3.0);
 	glBegin(GL_POINTS);
 	Vec4 color;
 	for(int x = 0; x < xSize; x++)
@@ -126,30 +116,68 @@ static void display()
 		}
 	}
 	glEnd();
+}
 
+void displayParticles()
+{
+	glPointSize(1.0);
 	glBegin(GL_POINTS);
 
 	Vec2 position, velocity;
+
+	glColor3f(1.0f, 1.0f, 1.0f);
 	for(int i = 0; i < numParticles; i++)
 	{
-		float r, g, b;
-		r = 1.0f;
-		g = 1.0f;
-		b = 1.0f;
-		Particle* cur = &particles[i];
+		Particle* cur;
+		cur = &particles[i];
 		position = cur -> getPosition();
 		velocity = cur -> getVelocity();
+
+		int lowX = floor(position.x);
+		int highX = ceil(position.x);
+		int lowY = floor(position.y);
+		int highY = ceil(position.y);
+
+		Vec2 downLeftForce = grid[lowX][lowY].force;
+		Vec2 downRightForce = grid[highX][lowY].force;
+		Vec2 upLeftForce = grid[lowX][highY].force;
+		Vec2 upRightForce = grid[highX][highY].force;
+
+		//float xOffset = position.x - lowX;
+		//float yOffset = position.y - lowY;
+
+		Vec2 force;
+		// TODO: replace
+		force.x = downLeftForce.x + downRightForce.x + upLeftForce.x + upRightForce.x;
+		force.y = downLeftForce.y + downRightForce.y + upLeftForce.y + upRightForce.y;
+		force.x /= 4;
+		force.y /= 4;
+
+		velocity = force;
+
 		if(position.x + velocity.x <= 0 || position.x + velocity.x >= xSize)
-			velocity.x = -velocity.x*2;
+			velocity.x = -velocity.x;
 		if(position.y + velocity.y <= 0 || position.y  + velocity.y >= ySize)
-			velocity.y = -velocity.y*2;
+			velocity.y = -velocity.y;
 
 		cur -> setPosition(position.x + velocity.x, position.y + velocity.y);
+		cur -> setVelocity(velocity.x, velocity.y);
 
-		glColor3f(r, g, b);
 		glVertex3f(position.x, position.y, 0);
 	}
 	glEnd();
+}
+
+/* Handler for window-repaint event. Call back when the window first appears and
+    whenever the window needs to be re-painted. */
+static void display()
+{
+	//glClearDepth(GL_DEPTH_TEST);
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color to black and opaque
+	glClear(GL_COLOR_BUFFER_BIT);         // Clear the color buffer
+
+	displayGrid();
+	displayParticles();
 
 
 	glVertex3f(32.0f, 32.0f, 0);
@@ -157,6 +185,7 @@ static void display()
 
 	glFlush();  // Render now
 	glutSwapBuffers();
+	glutTimerFunc(33, timer, 0);
 }
 
 /*
@@ -185,24 +214,8 @@ static void generateParticles()
 	}
 }
 
-/*
- * Main function: GLUT runs as a console application starting at main()
- */
-int main(int argc, char** argv)
+static void generateNodes()
 {
-	glutInit(&argc, argv); // Initialize GLUT
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
-	glutInitWindowSize(900, 900); // Set the window's initial width & height
-	glutInitWindowPosition(50, 50); // Position the window's initial top-left corner
-	glutCreateWindow("Snow Tiem"); // Create a window with the given title
-
-	glutDisplayFunc(display); // Register display callback handler for window re-paint
-	glutIdleFunc(idle); // Method called when we are idle.
-
-	// Provide a Perspective view of our scene.
-	gluPerspective(90.0f, 1.0f, 1.0f, -10.0f);
-	gluLookAt (xSize/2, ySize/2, 35.0f, xSize/2, ySize/2, -200.0f, 0.0f, 1.0f, 0.0f);
-
 	int x, y;
 	float r, g, b;
 	for(x = 0; x < xSize; x++)
@@ -214,10 +227,31 @@ int main(int argc, char** argv)
 			g = 0.2f + rand()%30/100.0f;
 			b = 0.2f + rand()%30/100.0f;
 			a.setRGBA(r, g, b, 1.0f);
-			a.setForce(0.0f, -0.001f);
+			a.setForce(rand()%30/10000.0f, rand()%30/10000.0f);
 			grid[x][y] = a;
 		}
 	}
+}
+
+/*
+ * Main function: GLUT runs as a console application starting at main()
+ */
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv); // Initialize GLUT
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(1600, 900); // Set the window's initial width & height
+	glutInitWindowPosition(50, 50); // Position the window's initial top-left corner
+	glutCreateWindow("Snow Tiem"); // Create a window with the given title
+
+	glutDisplayFunc(display); // Register display callback handler for window re-paint
+	glutIdleFunc(idle); // Method called when we are idle.
+
+	// Provide a Perspective view of our scene.
+	gluPerspective(90.0f, 1.0f, 1.0f, -10.0f);
+	gluLookAt(xSize/2, ySize/2, 35.0f, xSize/2, ySize/2, -200.0f, 0.0f, 1.0f, 0.0f);
+
+	generateNodes();
 
 	generateParticles();
 
