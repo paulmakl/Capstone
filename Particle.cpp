6fe416045f84bbfb70340ef4ecb9597bd7faa9aa
Etan Bard <ebard@pugetsound.edu>
2014-02-24 11:47:47 -0800

    Pseudo-Code for Collisions

--- Particle.cpp
+++ Particle.cpp
@@ -12,7 +12,6 @@ void Particle::setPosition(float x, float y)
     position.y = y;
     boxID.x = floor(x);
     boxID.y = floor(y);
-    
 }
 
 void Particle::setVelocity(float x, float y)
@@ -24,6 +23,11 @@ void Particle::setVelocity(float x, float y)
 void Particle::setMass(float newMass)
 {
 	mass = newMass;
+}
+
+void Particle:: setVolume(float newVolume)
+{
+	volume = newVolume;
 }
 
 void Particle::setColor(float r, float g, float b)
@@ -51,6 +55,11 @@ Vec3 Particle::getColor()
 float Particle::getMass()
 {
 	return mass;
+}
+
+float Particle::getVolume()
+{
+	return volume;
 }
 
 
