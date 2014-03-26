b7f1f31fd81487cb8b2ed9c308b04f7ab5a0cee1
paulmakl <paul@paulnmakl.com>
2014-03-26 08:56:08 -0700

    removed some of my changes, will merge later

---
+++ playback/playback/playback/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  playback
+//
+//  Created by Paul Makl on 3/24/14.
+//  Copyright (c) 2014 Paul Makl. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
